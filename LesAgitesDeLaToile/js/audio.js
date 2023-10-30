6fdfbc9dcd391f562c9b1af35ecf97c6c196d419
MorganeS117 <alexmoo@hotmail.fr>
2023-10-30 16:47:49 +0100

    erzerzer

---
+++ LesAgitesDeLaToile/js/audio.js
@@ -0,0 +1,92 @@
+let i = 0;
+let data;
+let doto;
+function init() {
+
+  var client_secret = 'eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiIzODFjYjA0MDk3YTUxNGJiNGE2MGIzODJlYWZjMzczOCIsInN1YiI6IjY1M2I4NGM3Y2M5NjgzMDEyY2YyZmU4NyIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ._Hzx0sesWpwKlhOdnONqoeInZsRFGedj4oqwg30GkRY';
+  const BASE_URL = 'https://api.themoviedb.org/3';
+  const API_URL = BASE_URL + '/movie/top_rated?language=en-US&page=1';
+
+
+  const options = {
+    method: 'GET',
+    headers: {
+      accept: 'application/json',
+      Authorization: 'Bearer ' + client_secret
+    }
+  };
+
+
+  fetch(API_URL, options)
+    .then(function(response) { return response.json() })
+    .then(responseData => {
+      data =responseData;
+      console.log(data);
+      for (let index = 0; index < data.results.length; index++) {
+
+        fetch(`https://api.themoviedb.org/3/movie/${data.results[index].id}/videos?language=en-US&page=1`, options)
+        .then(function(risponse) { return risponse.json() })
+        .then(risponseDoto => {
+          doto =risponseDoto;
+          console.log(doto);
+          const videos = doto.results;
+       
+       let trailerVideo = 0;
+       for (let i = 0; i < videos.length; i++) {
+         if (videos[i].name.toLowerCase().includes("trailer")) {
+           trailerVideo = videos[i];
+           break;
+         }
+       }
+
+       
+       if (trailerVideo) {
+         const videoKey = trailerVideo.key;
+         document.getElementById('modo').innerHTML += `
+           <button type="button" class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#Modale${index}">
+             <img src="https://www.themoviedb.org/t/p/w220_and_h330_face${data.results[index].poster_path}">
+           </button>
+
+           <div class="modal fade" id="Modale${index}" tabindex="-1" aria-labelledby="ModalLabel" aria-hidden="true">
+             <div class="modal-dialog modal-fullscreen">
+               <div class="modal-content">
+                 <div class="modal-header">
+                   <h5 class="modal-title" id="ModalLabel">${data.results[index].title}</h5>
+                 </div>
+                 <div class="modal-body">
+                   <p>${data.results[index].overview}</p>
+                   <iframe width="560" height="315" src="https://www.youtube.com/embed/${videoKey}" frameborder="0" allowfullscreen></iframe>
+                 </div>
+                 <div class="modal-footer">
+                   <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                 </div>
+               </div>
+             </div>
+           </div>
+         `;
+       }
+     });
+ }
+});
+    
+    
+
+
+}
+
+function search() {
+  let input = document.getElementById('searchbar').value
+  input = input.toLowerCase();
+  let x = document.getElementsByClassName('btn btn-danger');
+
+
+
+  for (j = 0; j < x.length; j++) {
+    if (!data.results[j].title.toLowerCase().includes(input)) {
+      x[j].style.display = "none";
+    }
+    else {
+      x[j].style.display = "inline";
+    }
+  }
+}
